d0a6558c7574f009605b0047dbff5fe1ff592151
Apoorva Anand <apoorvaanand.work@gmail.com>
2023-06-05 21:21:27 +0200

    Decode places.json to [Place]

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
+{-#LANGUAGE DeriveGeneric #-}
+
 module Main where
 
+import GHC.Generics
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+
+data Place = Place { city :: String } deriving (Show, Generic)
+
+instance FromJSON Place
+
+placesJsonFile :: FilePath
+placesJsonFile = "places.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile placesJsonFile
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  d <- (eitherDecode <$> getJSON) :: IO (Either String [Place])
+  case d of
+    Left err -> putStrLn err
+    Right ps -> print ps
