473da9c5fda97abd44e5f3ac0c8afbbe376cef9e
Apoorva Anand <apoorvaanand.work@gmail.com>
2023-06-05 21:46:58 +0200

    Decode products.json into [Product]

--- app/Main.hs
+++ app/Main.hs
@@ -7,18 +7,31 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy as B
 
 data Place = Place { city :: String } deriving (Show, Generic)
+data Product = Product { title :: String } deriving (Show, Generic)
 
 instance FromJSON Place
+instance FromJSON Product
 
 placesJsonFile :: FilePath
 placesJsonFile = "places.json"
 
-getJSON :: IO B.ByteString
-getJSON = B.readFile placesJsonFile
+productsJsonFile :: FilePath
+productsJsonFile = "products.json"
+
+getPlacesJSON :: IO B.ByteString
+getPlacesJSON = B.readFile placesJsonFile
+
+getProductsJSON :: IO B.ByteString
+getProductsJSON = B.readFile productsJsonFile
 
 main :: IO ()
 main = do
-  d <- (eitherDecode <$> getJSON) :: IO (Either String [Place])
-  case d of
-    Left err -> putStrLn err
-    Right ps -> print ps
+  dpl <- (eitherDecode <$> getPlacesJSON) :: IO (Either String [Place])
+  dpr <- (eitherDecode <$> getProductsJSON) :: IO (Either String [Product])
+  case (dpl, dpr) of
+    (Left e1, Left e2)     -> putStrLn (e1 ++ e2)
+    (Left e1, _)           -> putStrLn e1
+    (_, Left e2)           -> putStrLn e2
+    (Right pls, Right prs) -> do
+                                print pls
+                                print prs
