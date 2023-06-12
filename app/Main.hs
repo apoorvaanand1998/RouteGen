8ed48f57131210f837c9fac20b7af3d93e0e4f93
Apoorva Anand <apoorvaanand.work@gmail.com>
2023-06-12 12:50:46 +0200

    Change Route for slightly better json

--- app/Main.hs
+++ app/Main.hs
@@ -15,9 +15,9 @@ data Product = Product { title :: String } deriving (Eq, Show, Generic)
 
 data Trip = Trip { fromCity    :: Place
                  , toCity      :: Place
-                 , merchandise :: [(Product, Int)] } deriving (Show, Generic)
+                 , merchandise :: [(Product, Int)] } deriving (Eq, Show, Generic)
 
-type Route    = [Trip]
+newtype Route = Route { theRoute :: [Trip] } deriving (Eq, Show, Generic)
 type Places   = [Place]
 type Products = [Product]
 
@@ -26,6 +26,7 @@ instance FromJSON Product
 instance ToJSON Place
 instance ToJSON Product
 instance ToJSON Trip
+instance ToJSON Route
 
 placesJsonFile :: FilePath
 placesJsonFile = "places.json"
@@ -36,7 +37,7 @@ productsJsonFile = "products.json"
 testRouteJsonFile :: FilePath
 testRouteJsonFile = "testRoute.json"
 
-createTest :: Route -> IO ()
+createTest :: [Route] -> IO ()
 createTest = encodeFile testRouteJsonFile
 
 getPlacesJSON :: IO B.ByteString
@@ -88,17 +89,17 @@ generateStandardRoute :: Places -> Products -> Gen Route
 generateStandardRoute pls prs = do
   seedTrip <- generateRandomTrip pls prs
   times <- choose (3, 10)
-  helperRes <- generateStandardRoute' times [] seedTrip [seedTrip]
-  return $ reverse $ fst helperRes
+  helperRes <- generateStandardRoute' times [] seedTrip (Route [seedTrip])
+  return $ Route (reverse $ theRoute $ fst helperRes)
   where
     generateStandardRoute' :: Int -> Places -> Trip -> Route -> Gen (Route, Places)
     generateStandardRoute' 0 alreadyPlaces _ route            = return (route, alreadyPlaces)
     generateStandardRoute' n alreadyPlaces seedTrip prevRoute = do
       (nextTrip, alreadyPlaces') <- generateNextTrip pls prs alreadyPlaces seedTrip
-      generateStandardRoute' (n-1) alreadyPlaces' nextTrip (nextTrip : prevRoute)
+      generateStandardRoute' (n-1) alreadyPlaces' nextTrip (Route (nextTrip : theRoute prevRoute))
 
 getAlreadyPlaces :: Route -> Places
-getAlreadyPlaces = undefined 
+getAlreadyPlaces r = foldr (\ x -> (++) [fromCity x, toCity x]) [] (theRoute r)
 
 generateActualRoute :: Places -> Products -> Route -> Gen Route
 generateActualRoute pls prs stdRoute = do
@@ -107,7 +108,7 @@ generateActualRoute pls prs stdRoute = do
   return []
   where
     addTrips :: [Int] -> Route -> Route
-    addTrips addIndices r = undefined
+    addTrips addIndices r = undefined 
 
 randomStandardRoute :: Int -> IO Route
 randomStandardRoute seed = do
@@ -125,3 +126,11 @@ main = do
     (Right pls, Right prs) -> do
                                 print pls
                                 print prs
+
+{-
+testRoute1 = Route [Trip {fromCity = Place {city = "ChennaiHAHA"}, toCity = Place {city = "Houten"}, merchandise = [(Product {title = "Green smoothie"},57),(Product {title = "Baking cake"},394),(Product {title = "Homemade bread"},84),(Product {title = "Healthy breakfast"},361),(Product {title = "Green beans"},89)]},Trip {fromCity = Place {city = "Houten"}, toCity = Place {city = "Bolnes"}, merchandise = [(Product {title = "Green smoothie"},57),(Product {title = "Homemade bread"},84),(Product {title = "Healthy breakfast"},361),(Product {title = "Brown eggs"},59),(Product {title = "Lemon and salt"},157)]},Trip {fromCity = Place {city = "Bolnes"}, toCity = Place {city = "Hoensbroek"}, merchandise = [(Product {title = "Homemade bread"},84),(Product {title = "Brown eggs"},59),(Product {title = "Lemon and salt"},157),(Product {title = "Sweet fresh stawberry"},90),(Product {title = "Hazelnut in black ceramic bowl"},392),(Product {title = "Legums"},392)]},Trip {fromCity = Place {city = "Hoensbroek"}, toCity = Place {city = "Wageningen"}, merchandise = [(Product {title = "Brown eggs"},59),(Product {title = "Lemon and salt"},157),(Product {title = "Hazelnut in black ceramic bowl"},392),(Product {title = "Legums"},392)]},Trip {fromCity = Place {city = "Wageningen"}, toCity = Place {city = "Kwintsheul"}, merchandise = [(Product {title = "Lemon and salt"},157),(Product {title = "Legums"},392)]},Trip {fromCity = Place {city = "Kwintsheul"}, toCity = Place {city = "Waalre"}, merchandise = [(Product {title = "Legums"},392)]},Trip {fromCity = Place {city = "Waalre"}, toCity = Place {city = "Brunssum"}, merchandise = [(Product {title = "Legums"},392)]},Trip {fromCity = Place {city = "Brunssum"}, toCity = Place {city = "Surhuisterveen"}, merchandise = [(Product {title = "Legums"},392)]}]
+testRoute2 = Route [Trip {fromCity = Place {city = "Badhoevedorp"}, toCity = Place {city = "Landgraaf"}, merchandise = [(Product {title = "Brown eggs"},178),(Product {title = "Sweet fresh stawberry"},18),(Product {title = "Asparagus"},408),(Product {title = "Green smoothie"},255),(Product {title = "Raw legums"},62)]},Trip {fromCity = Place {city = "Landgraaf"}, toCity = Place {city = "Berghem"}, merchandise = [(Product {title = "Sweet fresh stawberry"},18),(Product {title = "Asparagus"},408),(Product {title = "Green smoothie"},255),(Product {title = "Raw legums"},62),(Product {title = "Green smoothie"},317),(Product {title = "Raw legums"},190),(Product {title = "Pesto with basil"},138)]},Trip {fromCity = Place {city = "Berghem"}, toCity = Place {city = "Rijsenhout"}, merchandise = [(Product {title = "Green smoothie"},255),(Product {title = "Raw legums"},62),(Product {title = "Pesto with basil"},138)]},Trip {fromCity = Place {city = "Rijsenhout"}, toCity = Place {city = "Zuid-Scharwoude"}, merchandise = [(Product {title = "Green smoothie"},255)]},Trip {fromCity = Place {city = "Zuid-Scharwoude"}, toCity = Place {city = "Amersfoort"}, merchandise = [(Product {title = "Green smoothie"},255)]},Trip {fromCity = Place {city = "Amersfoort"}, toCity = Place {city = "Heer"}, merchandise = [(Product {title = "Green smoothie"},255)]},Trip {fromCity = Place {city = "Heer"}, toCity = Place {city = "Huissen"}, merchandise = [(Product {title = "Green smoothie"},255),(Product {title = "Pesto with basil"},204)]},Trip {fromCity = Place {city = "Huissen"}, toCity = Place {city = "Nieuwegein"}, merchandise = [(Product {title = "Green smoothie"},169),(Product {title = "Raw legums"},411)]},Trip {fromCity = Place {city = "Nieuwegein"}, toCity = Place {city = "Goutum"}, merchandise = [(Product {title = "Green smoothie"},169),(Product {title = "Raw legums"},411)]},Trip {fromCity = Place {city = "Goutum"}, toCity = Place {city = "Kerkrade"}, merchandise = [(Product {title = "Raw legums"},411),(Product {title = "Raw legums"},385)]},Trip {fromCity = Place {city = "Kerkrade"}, toCity = Place {city = "Vleuten"}, merchandise = [(Product {title = "Raw legums"},385)]}]
+testRoute3 = Route [Trip {fromCity = Place {city = "Zwolle"}, toCity = Place {city = "Uithoorn"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Asparagus"},420),(Product {title = "Green smoothie"},39),(Product {title = "Raw legums"},64)]},Trip {fromCity = Place {city = "Uithoorn"}, toCity = Place {city = "Utrecht"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Asparagus"},420),(Product {title = "Green smoothie"},39)]},Trip {fromCity = Place {city = "Utrecht"}, toCity = Place {city = "Lelystad"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Asparagus"},420),(Product {title = "Green smoothie"},39)]},Trip {fromCity = Place {city = "Lelystad"}, toCity = Place {city = "Tilburg"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Green smoothie"},39)]},Trip {fromCity = Place {city = "Tilburg"}, toCity = Place {city = "Empel"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Green smoothie"},39)]},Trip {fromCity = Place {city = "Empel"}, toCity = Place {city = "\8217s-Gravenzande"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Green smoothie"},39),(Product {title = "Hazelnut in black ceramic bowl"},21),(Product {title = "Fresh stawberry"},232),(Product {title = "Homemade bread"},408)]},Trip {fromCity = Place {city = "\8217s-Gravenzande"}, toCity = Place {city = "Nieuwegein"}, merchandise = [(Product {title = "Sweet fresh stawberry"},19),(Product {title = "Green smoothie"},39),(Product {title = "Hazelnut in black ceramic bowl"},21)]},Trip {fromCity = Place {city = "Nieuwegein"}, toCity = Place {city = "Weesp"}, merchandise = [(Product {title = "Sweet fresh stawberry"},292)]},Trip {fromCity = Place {city = "Weesp"}, toCity = Place {city = "Beek"}, merchandise = [(Product {title = "Sweet fresh stawberry"},292),(Product {title = "Brown eggs"},157)]}]
+
+testRoutes = [testRoute1, testRoute2, testRoute3]
+-}
